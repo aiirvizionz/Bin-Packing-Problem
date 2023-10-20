1c283ff186a3800d35ffae3a298c180cf65a1167
aiirvizionz <david.lopezhuerta01@gmail.com>
2023-10-19 20:28:53 -0600

    3er avance, se agregó forma de controlar la creacion de figuras y contenedores mediante inputs de text, mejora al arrastrado de figuras y cambio en los colores

--- app.js
+++ app.js
@@ -1,57 +1,3 @@
-//Funcion que cambia atributos de CCS cuando se arrastra un elemento
-function onDragStart(event) {
-    event
-        .dataTransfer
-        .setData('text/plain', event.target.id);
-    event
-        .currentTarget
-        .style
-        .opacity = '0.7';
-    /* event
-     .currentTarget
-     .style
-     .scale= '1.5'; */
-}
-
-function onDragOver(event) {
-    event.preventDefault();
-}
-
-
-function onDrop(event) {
-    const id = event
-        .dataTransfer
-        .getData('text');
-
-    let currentDivIndex = 0; // Índice del div actual que se debe mostrar
-
-    const binToAdd = document.querySelector('.carga');
-    const containers = binToAdd.querySelectorAll('.containerToAdd');
-    const draggableElement = document.getElementById(id);
-
-    const dropzone = event.target;
-
-    if (draggableElement && containers[currentDivIndex]) {
-        // Eliminar el contenedor actual
-        containers[currentDivIndex].remove();
-
-        currentDivIndex++;
-        figuresInfoDropped++;
-
-        if (currentDivIndex < containers.length) {
-            binToAdd.appendChild(containers[currentDivIndex]);
-        }
-    }
-
-    dropzone.appendChild(draggableElement);
-
-    event
-        .dataTransfer
-        .clearData();
-}
-
-
-
 // Checkbox para mostrar pantalla informativa sobre el juego
 const checkbox = document.getElementById("cb-menu")
 const infoRules = document.getElementById("intro");
@@ -96,74 +42,92 @@ function showSlides(n) {
 }
 
 //Listas para valores de colores y alturas de las figuras
-const colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange', 'pink', 'brown', 'black', 'cian', 'gray', 'white'];
+
+const colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange', 'pink', 'brown', 'black', 'cian', 'gray'];
+
+
 
 //Listas de los datos asignados a cada figura
+
 const heights = [];
 const colorsAssigned = [];
 
-//Logica para la creacion de figuras con colores y alturas aleatorias
-
-
-console.log("La cantidad de colores son: " + colors.length);
-console.log(heights);
 
 
 //Logica para la creacion de contenedores y figuras arrastrables que el jugador eligio
-const figures = 20//prompt("Ingrese el número de contenedores que desea acomodar: ");
-const sizeFigures = parseInt(figures, 10);
 
-if (isNaN(sizeFigures) || sizeFigures <= 0) {
-    alert("Ingrese un número válido mayor que cero.");
-} else {
-    const binToAdd = document.querySelector('.carga');
-
-    for (let i = 1; i <= sizeFigures; i++) {
-        /*const newContainer = document.createElement('div');
-        newContainer.setAttribute('class', 'containerToAdd');
-
-        binToAdd.appendChild(newContainer);
-
-        const newDiv = document.createElement('div');
-        newDiv.setAttribute('class', 'figure');
-        newDiv.setAttribute('draggable', 'true');
-        newDiv.setAttribute('ondragstart', 'onDragStart(event);');
-        newDiv.setAttribute('id', 'draggable-' + i);
-        /*newDiv.textContent = 'Arrastrable ' + i; */
-
-        /* newContainer.appendChild(newDiv);
-        console.log(newDiv);  */
-
-        const randomColor = Math.floor(Math.random() * colors.length);
-        const randomHeight = Math.floor(Math.random() * 300);
-
-        console.log(randomColor);
-        console.log(randomHeight);
-
-        const newDiv = document.createElement('div');
-        newDiv.setAttribute('class', 'figure');
-        newDiv.setAttribute('draggable', 'true');
-        newDiv.setAttribute('ondragstart', 'onDragStart(event);');
-        newDiv.setAttribute('id', 'draggable-' + i);
-        newDiv.style.backgroundColor = colors[randomColor];
-        newDiv.style.height = randomHeight + 'px';
-        newDiv.textContent = randomHeight;
-
-        const binToAdd = document.querySelector('.carga');
-        const newContainer = document.createElement('div');
-        newContainer.setAttribute('class', 'containerToAdd');
-
-        binToAdd.appendChild(newContainer);
-        newContainer.appendChild(newDiv);
+const inputFigures = document.getElementById("inputFigures");
+const btnFigures = document.getElementById("btnFigures");
 
 
-        heights.push(randomHeight);
-        colorsAssigned.push(randomColor);
+btnFigures.addEventListener("click", function () {
+
+    if (isNaN(inputFigures.value) || inputFigures.value <= 0) {
+        alert("Ingrese un número válido mayor que cero.");
+    } else {
+        binToAdd.innerHTML = '';
+        for (let i = 1; i <= inputFigures.value; i++) {
+            const randomColor = Math.floor((Math.random() * colors.length) + 1);
+            const randomHeight = Math.floor(Math.random() * 300);
+
+            const newDiv = document.createElement('div');
+            newDiv.setAttribute('class', 'figure');
+            newDiv.setAttribute('draggable', 'true');
+            newDiv.setAttribute('ondragstart', 'onDragStart(event);');
+            newDiv.setAttribute('id', 'draggable-' + i);
+            newDiv.style.backgroundColor = colors[randomColor];
+            newDiv.style.height = randomHeight + 'px';
+            newDiv.textContent = randomHeight;
+
+            const binToAdd = document.querySelector('.carga');
+            binToAdd.appendChild(newDiv);
+
+            heights.push(randomHeight);
+            colorsAssigned.push(randomColor);
+
+            const figuresCreated = binToAdd.childElementCount;
+            figuresCreatedInfo.textContent = 'Cantidad de figuras creadas: ' + figuresCreated;
+        }
     }
+    console.log(inputFigures.value);
+});
+
+const inputBins = document.getElementById("inputBins");
+const btnBins = document.getElementById("btnBins");
+
+btnBins.addEventListener("click", function () {
+
+    if (cash >= 10) {
+        for (let i = 1; i <= inputBins.value; i++) {
+            const newBin = document.createElement('div');
+            newBin.setAttribute('class', 'bin');
+            newBin.setAttribute('ondragover', 'onDragOver(event);');
+            newBin.setAttribute('ondrop', 'onDrop(event);');
+
+            containerBins.appendChild(newBin);
+
+            console.log(newBin);
+
+            cash = cash - 10;
+            cashInfo.textContent = 'Dinero disponible: $' + cash;
+        }
+    } else {
+        alert("No se pueden agregar más contenedores.");
+        return;
+    }
+});
+
+
+function onDragStart(event) {
+    event.dataTransfer.setData("text/plain", event.target.id);
 }
+
 
 console.log(heights);
 console.log(colorsAssigned);
+
+
+
 
 //Informacion del juego
 const divInfoGame = document.querySelector(".info-game");
@@ -175,7 +139,7 @@ var figuresInfoDropped = 0;
 var cash = 100;
 
 const binsInfo = document.getElementById("used-containers-info");
-figuresCreatedInfo.textContent = 'Cantidad de figuras creadas: ' + sizeFigures;
+figuresCreatedInfo.textContent = 'Cantidad de figuras creadas: ';
 cashInfo.textContent = 'Dinero disponible: $' + cash;
 //figuresInfo.textContent = 'Cantidad de figuras acomodadas: ' + figuresInfoDropped;
 //binsInfo.textContent = 'Cantidad de contenedores usados: '; 
@@ -187,25 +151,76 @@ const containerBins = document.querySelector(".binChildToReceive");
 var indexBins = 1;
 
 
-console.log(btnAddBin);
 
-btnAddBin.addEventListener("click", function () {
 
-    if (indexBins > 5) {
-        alert("No se pueden agregar más contenedores.");
-        return;
-    } else {
-        const newBin = document.createElement('div');
-        newBin.setAttribute('class', 'bin');
-        newBin.setAttribute('ondragover', 'onDragOver(event);');
-        newBin.setAttribute('ondrop', 'onDrop(event);');
 
-        containerBins.appendChild(newBin);
+//Funcion que cambia atributos de CCS cuando se arrastra un elemento
+function onDragStart(event) {
+    event
+        .dataTransfer
+        .setData('text/plain', event.target.id);
+    event
+        .currentTarget
+        .style
+        .opacity = '0.7';
+    /* event
+     .currentTarget
+     .style
+     .scale= '1.5'; */
+}
 
-        console.log(newBin);
-        indexBins++;
-        cash = cash - 10;
-        cashInfo.textContent = 'Dinero disponible: $' + cash;
+function onDragOver(event) {
+    event.preventDefault();
+}
 
+// Contador para el número de capacidad ocupada en el contenedor
+let elementosEnContenedor = 0;
+// Límite de capacidad del contenedor
+const limiteCapacidad = 500;
+
+
+function onDrop(event) {
+    const id = event
+        .dataTransfer
+        .getData('text');
+
+    let currentDivIndex = 0; // Índice del div actual que se debe mostrar
+
+    const binToAdd = document.querySelector('.carga');
+    const containers = binToAdd.querySelectorAll('.containerToAdd');
+    const draggableElement = document.getElementById(id);
+
+    const dropzone = event.target;
+
+    if (draggableElement.textContent <= limiteCapacidad) {
+
+
+        if (draggableElement && containers[currentDivIndex]) {
+
+
+
+            if (currentDivIndex < containers.length) {
+                binToAdd.appendChild(containers[currentDivIndex]);
+
+                currentDivIndex++;
+                figuresInfoDropped++;
+
+                limiteCapacidad = limiteCapacidad - draggableElement.textContent;
+                console.log(limiteCapacidad);
+
+
+            }
+        }
+
+        dropzone.appendChild(draggableElement);
+
+
+        event
+            .dataTransfer
+            .clearData();
     }
-});
+}
+
+function allowDrop(event) {
+    event.preventDefault();
+}
